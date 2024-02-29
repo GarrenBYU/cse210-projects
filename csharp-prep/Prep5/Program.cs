5058b42f56ba598e9de5c93ac485c8e4dc150f80
Garren Meisman <garren@meisman.com>
2024-02-29 02:57:16 -0700

    Process of step 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayMessage ()
+        {
+        Console.WriteLine("Welcome to the program!");
+        }
+        static void PromptUserName ()
+        {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        }
+        static void PromptUserNumber ()
+        {
+        Console.Write("Please enter your favorite number: ");
+        string number = Console.ReadLine();
+        }
+        static void SquareNumber ()
+        {
+        int square = int.Parse(number) * int.Parse(number);
+        }
+        static void DisplayResult ()
+        {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+        }
     }
 }
